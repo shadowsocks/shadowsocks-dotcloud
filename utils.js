c6f484c1323c085a70b01d15ff35d47f8a0bf1e3
Zhao Xiaohong <mrluanma@gmail.com>
2024-03-10 13:52:50 +0800

    Simplify IPv6 inetNtoa.

--- utils.js
+++ utils.js
@@ -1,12 +1,11 @@
 export function inetNtoa(family, buf) {
   if (family === 4) return buf[0] + '.' + buf[1] + '.' + buf[2] + '.' + buf[3];
   else if (family === 6) {
-    let str = Buffer.alloc(0);
+    let addr = [];
     for (let i = 0; i < 8; i++) {
-      str += buf.readUInt16BE(i * 2, i * 2 + 2).toString(16);
-      if (i < 7) str += ':';
+      addr.push(buf.readUInt16BE(i * 2, i * 2 + 2).toString(16));
     }
-    return str;
+    return addr.join(':');
   }
 }
 
